d3f90758da659475d751aca256ee71b5cbdb4458
Fahd <fahd.addi@gmail.com>
2020-06-14 16:31:13 +0100

    static data declaration

---
+++ data/index.js
@@ -0,0 +1,582 @@
+import Product from '~/Models/Product';
+
+
+export const Products = [
+    new Product(1, '12 x Protein Snack 35 g', 11.88, 50, 'https://static.sscontent.com/prodimg/thumb/832/832/products/124/v669261_prozis_12-x-protein-snack-35-g_vanilla_newin_flavor.jpg', `Avertissement : Nous conseillons de consulter un médecin ou un nutritionniste en cas de doute sur la posologie et/ou l'utilisation de ce produit. Les informations nutritionnelles peuvent varier selon le parfum du produit.  * Les images présentées ont un but purement illustratif et promotionnel. L'image finale du produit peut être modifiée.`, [{
+            parfum: 'Chocolat',
+            energie: {
+                percent: 7,
+                value: 138
+            },
+            matieresGrasses: {
+                value: 4.36,
+                percent: 6,
+                childs: [{
+                    title: 'Acides Gras Saturés',
+                    percent: 13,
+                    value: 2.63,
+                }]
+            },
+            glucides: {
+                value: 15.05,
+                percent: 6,
+                childs: [{
+                        title: 'Sucres',
+                        percent: 14,
+                        value: 12.95,
+                    },
+                    {
+                        title: 'Polyols',
+                        percent: '**',
+                        value: 1.58
+                    },
+                    {
+                        title: 'Fibres Alimentaires',
+                        percent: '**',
+                        value: 1.68
+                    }
+                ]
+            },
+            proteines: {
+                value: 9.59,
+                percent: 19,
+            },
+            sel: {
+                percent: 1,
+                value: 0.07,
+            }
+
+        },
+        {
+            parfum: 'Vanille',
+            energie: {
+                percent: 7,
+                value: 138
+            },
+            matieresGrasses: {
+                value: 4.55,
+                percent: 7,
+                childs: [{
+                    title: 'Acides Gras Saturés',
+                    percent: 14,
+                    value: 2.77,
+                }]
+            },
+            glucides: {
+                value: 14.70,
+                percent: 6,
+                childs: [{
+                        title: 'Sucres',
+                        percent: 14,
+                        value: 12.95,
+                    },
+                    {
+                        title: 'Polyols',
+                        percent: '**',
+                        value: 1.54
+                    },
+                    {
+                        title: 'Fibres Alimentaires',
+                        percent: '**',
+                        value: 2.03
+                    }
+                ]
+            },
+            proteines: {
+                value: 9.45,
+                percent: 19,
+            },
+            sel: {
+                percent: 1,
+                value: 0.07,
+            }
+
+        },
+        {
+            parfum: 'Cookies et crème',
+            energie: {
+                percent: 7,
+                value: 137.55
+            },
+            matieresGrasses: {
+                value: 4.20,
+                percent: 6,
+                childs: [{
+                    title: 'Acides Gras Saturés',
+                    percent: 13,
+                    value: 2.59,
+                }]
+            },
+            glucides: {
+                value: 15.40,
+                percent: 6,
+                childs: [{
+                        title: 'Sucres',
+                        percent: 15,
+                        value: 13.30,
+                    },
+                    {
+                        title: 'Polyols',
+                        percent: '**',
+                        value: 1.68
+                    },
+                    {
+                        title: 'Fibres Alimentaires',
+                        percent: '**',
+                        value: 2.03
+                    }
+                ]
+            },
+            proteines: {
+                value: 9.17,
+                percent: 18,
+            },
+            sel: {
+                percent: 1,
+                value: 0.07,
+            }
+        }
+    ]),
+    new Product(2, 'Zero Snack 35 g', 1.29, 50, 'https://static.sscontent.com/prodimg/thumb/408/408/products/124/v791530_prozis_zero-snack-35-g_4.jpg', `Avertissement : Nous conseillons de consulter un médecin ou un nutritionniste en cas de doute sur la posologie et/ou l'utilisation de ce produit. Les informations nutritionnelles peuvent varier selon le parfum du produit.  * Les images présentées ont un but purement illustratif et promotionnel. L'image finale du produit peut être modifiée.`, [{
+            parfum: 'Cookies et crème',
+            energie: {
+                percent: 6,
+                value: 115.40
+            },
+            matieresGrasses: {
+                value: 3.29,
+                percent: 5,
+                childs: [{
+                    title: 'Acides Gras Saturés',
+                    percent: 6,
+                    value: 1.26,
+                }]
+            },
+            glucides: {
+                value: 7.35,
+                percent: 3,
+                childs: [{
+                        title: 'Sucres',
+                        percent: 3,
+                        value: 2.52,
+                    },
+                    {
+                        title: 'Polyols',
+                        percent: '**',
+                        value: 3.26
+                    }
+                ]
+            },
+            proteines: {
+                value: 11.55,
+                percent: 23,
+            },
+            sel: {
+                percent: 3,
+                value: 0.15,
+            }
+
+        },
+        {
+            parfum: 'Vanille',
+            energie: {
+                percent: 7,
+                value: 138
+            },
+            matieresGrasses: {
+                value: 4.55,
+                percent: 7,
+                childs: [{
+                    title: 'Acides Gras Saturés',
+                    percent: 14,
+                    value: 2.77,
+                }]
+            },
+            glucides: {
+                value: 14.70,
+                percent: 6,
+                childs: [{
+                        title: 'Sucres',
+                        percent: 14,
+                        value: 12.95,
+                    },
+                    {
+                        title: 'Polyols',
+                        percent: '**',
+                        value: 1.54
+                    },
+                    {
+                        title: 'Fibres Alimentaires',
+                        percent: '**',
+                        value: 2.03
+                    }
+                ]
+            },
+            proteines: {
+                value: 9.45,
+                percent: 19,
+            },
+            sel: {
+                percent: 1,
+                value: 0.07,
+            }
+
+        }
+    ]),
+    new Product(3, '12 x Protein Snack 35 g', 11.88, 50, 'https://static.sscontent.com/prodimg/thumb/832/832/products/124/v669261_prozis_12-x-protein-snack-35-g_vanilla_newin_flavor.jpg', `Avertissement : Nous conseillons de consulter un médecin ou un nutritionniste en cas de doute sur la posologie et/ou l'utilisation de ce produit. Les informations nutritionnelles peuvent varier selon le parfum du produit.  * Les images présentées ont un but purement illustratif et promotionnel. L'image finale du produit peut être modifiée.`, [{
+            parfum: 'Chocolat',
+            energie: {
+                percent: 7,
+                value: 138
+            },
+            matieresGrasses: {
+                value: 4.36,
+                percent: 6,
+                childs: [{
+                    title: 'Acides Gras Saturés',
+                    percent: 13,
+                    value: 2.63,
+                }]
+            },
+            glucides: {
+                value: 15.05,
+                percent: 6,
+                childs: [{
+                        title: 'Sucres',
+                        percent: 14,
+                        value: 12.95,
+                    },
+                    {
+                        title: 'Polyols',
+                        percent: '**',
+                        value: 1.58
+                    },
+                    {
+                        title: 'Fibres Alimentaires',
+                        percent: '**',
+                        value: 1.68
+                    }
+                ]
+            },
+            proteines: {
+                value: 9.59,
+                percent: 19,
+            },
+            sel: {
+                percent: 1,
+                value: 0.07,
+            }
+
+        },
+        {
+            parfum: 'Vanille',
+            energie: {
+                percent: 7,
+                value: 138
+            },
+            matieresGrasses: {
+                value: 4.55,
+                percent: 7,
+                childs: [{
+                    title: 'Acides Gras Saturés',
+                    percent: 14,
+                    value: 2.77,
+                }]
+            },
+            glucides: {
+                value: 14.70,
+                percent: 6,
+                childs: [{
+                        title: 'Sucres',
+                        percent: 14,
+                        value: 12.95,
+                    },
+                    {
+                        title: 'Polyols',
+                        percent: '**',
+                        value: 1.54
+                    },
+                    {
+                        title: 'Fibres Alimentaires',
+                        percent: '**',
+                        value: 2.03
+                    }
+                ]
+            },
+            proteines: {
+                value: 9.45,
+                percent: 19,
+            },
+            sel: {
+                percent: 1,
+                value: 0.07,
+            }
+
+        },
+        {
+            parfum: 'Cookies et crème',
+            energie: {
+                percent: 7,
+                value: 137.55
+            },
+            matieresGrasses: {
+                value: 4.20,
+                percent: 6,
+                childs: [{
+                    title: 'Acides Gras Saturés',
+                    percent: 13,
+                    value: 2.59,
+                }]
+            },
+            glucides: {
+                value: 15.40,
+                percent: 6,
+                childs: [{
+                        title: 'Sucres',
+                        percent: 15,
+                        value: 13.30,
+                    },
+                    {
+                        title: 'Polyols',
+                        percent: '**',
+                        value: 1.68
+                    },
+                    {
+                        title: 'Fibres Alimentaires',
+                        percent: '**',
+                        value: 2.03
+                    }
+                ]
+            },
+            proteines: {
+                value: 9.17,
+                percent: 18,
+            },
+            sel: {
+                percent: 1,
+                value: 0.07,
+            }
+        }
+    ]),
+    new Product(4, 'Zero Snack 35 g', 1.29, 50, 'https://static.sscontent.com/prodimg/thumb/408/408/products/124/v791530_prozis_zero-snack-35-g_4.jpg', `Avertissement : Nous conseillons de consulter un médecin ou un nutritionniste en cas de doute sur la posologie et/ou l'utilisation de ce produit. Les informations nutritionnelles peuvent varier selon le parfum du produit.  * Les images présentées ont un but purement illustratif et promotionnel. L'image finale du produit peut être modifiée.`, [{
+            parfum: 'Cookies et crème',
+            energie: {
+                percent: 6,
+                value: 115.40
+            },
+            matieresGrasses: {
+                value: 3.29,
+                percent: 5,
+                childs: [{
+                    title: 'Acides Gras Saturés',
+                    percent: 6,
+                    value: 1.26,
+                }]
+            },
+            glucides: {
+                value: 7.35,
+                percent: 3,
+                childs: [{
+                        title: 'Sucres',
+                        percent: 3,
+                        value: 2.52,
+                    },
+                    {
+                        title: 'Polyols',
+                        percent: '**',
+                        value: 3.26
+                    }
+                ]
+            },
+            proteines: {
+                value: 11.55,
+                percent: 23,
+            },
+            sel: {
+                percent: 3,
+                value: 0.15,
+            }
+
+        },
+        {
+            parfum: 'Vanille',
+            energie: {
+                percent: 7,
+                value: 138
+            },
+            matieresGrasses: {
+                value: 4.55,
+                percent: 7,
+                childs: [{
+                    title: 'Acides Gras Saturés',
+                    percent: 14,
+                    value: 2.77,
+                }]
+            },
+            glucides: {
+                value: 14.70,
+                percent: 6,
+                childs: [{
+                        title: 'Sucres',
+                        percent: 14,
+                        value: 12.95,
+                    },
+                    {
+                        title: 'Polyols',
+                        percent: '**',
+                        value: 1.54
+                    },
+                    {
+                        title: 'Fibres Alimentaires',
+                        percent: '**',
+                        value: 2.03
+                    }
+                ]
+            },
+            proteines: {
+                value: 9.45,
+                percent: 19,
+            },
+            sel: {
+                percent: 1,
+                value: 0.07,
+            }
+
+        }
+    ]),
+    new Product(5, '12 x Protein Snack 35 g', 11.88, 50, 'https://static.sscontent.com/prodimg/thumb/832/832/products/124/v669261_prozis_12-x-protein-snack-35-g_vanilla_newin_flavor.jpg', `Avertissement : Nous conseillons de consulter un médecin ou un nutritionniste en cas de doute sur la posologie et/ou l'utilisation de ce produit. Les informations nutritionnelles peuvent varier selon le parfum du produit.  * Les images présentées ont un but purement illustratif et promotionnel. L'image finale du produit peut être modifiée.`, [{
+            parfum: 'Chocolat',
+            energie: {
+                percent: 7,
+                value: 138
+            },
+            matieresGrasses: {
+                value: 4.36,
+                percent: 6,
+                childs: [{
+                    title: 'Acides Gras Saturés',
+                    percent: 13,
+                    value: 2.63,
+                }]
+            },
+            glucides: {
+                value: 15.05,
+                percent: 6,
+                childs: [{
+                        title: 'Sucres',
+                        percent: 14,
+                        value: 12.95,
+                    },
+                    {
+                        title: 'Polyols',
+                        percent: '**',
+                        value: 1.58
+                    },
+                    {
+                        title: 'Fibres Alimentaires',
+                        percent: '**',
+                        value: 1.68
+                    }
+                ]
+            },
+            proteines: {
+                value: 9.59,
+                percent: 19,
+            },
+            sel: {
+                percent: 1,
+                value: 0.07,
+            }
+
+        },
+        {
+            parfum: 'Vanille',
+            energie: {
+                percent: 7,
+                value: 138
+            },
+            matieresGrasses: {
+                value: 4.55,
+                percent: 7,
+                childs: [{
+                    title: 'Acides Gras Saturés',
+                    percent: 14,
+                    value: 2.77,
+                }]
+            },
+            glucides: {
+                value: 14.70,
+                percent: 6,
+                childs: [{
+                        title: 'Sucres',
+                        percent: 14,
+                        value: 12.95,
+                    },
+                    {
+                        title: 'Polyols',
+                        percent: '**',
+                        value: 1.54
+                    },
+                    {
+                        title: 'Fibres Alimentaires',
+                        percent: '**',
+                        value: 2.03
+                    }
+                ]
+            },
+            proteines: {
+                value: 9.45,
+                percent: 19,
+            },
+            sel: {
+                percent: 1,
+                value: 0.07,
+            }
+
+        },
+        {
+            parfum: 'Cookies et crème',
+            energie: {
+                percent: 7,
+                value: 137.55
+            },
+            matieresGrasses: {
+                value: 4.20,
+                percent: 6,
+                childs: [{
+                    title: 'Acides Gras Saturés',
+                    percent: 13,
+                    value: 2.59,
+                }]
+            },
+            glucides: {
+                value: 15.40,
+                percent: 6,
+                childs: [{
+                        title: 'Sucres',
+                        percent: 15,
+                        value: 13.30,
+                    },
+                    {
+                        title: 'Polyols',
+                        percent: '**',
+                        value: 1.68
+                    },
+                    {
+                        title: 'Fibres Alimentaires',
+                        percent: '**',
+                        value: 2.03
+                    }
+                ]
+            },
+            proteines: {
+                value: 9.17,
+                percent: 18,
+            },
+            sel: {
+                percent: 1,
+                value: 0.07,
+            }
+        }
+    ]),
+]
