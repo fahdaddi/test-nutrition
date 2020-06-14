ba774575549ee76eeaec0d57b839e7e351b9aa20
Fahd <fahd.addi@gmail.com>
2020-06-14 16:32:11 +0100

    added product card component

---
+++ components/itemCard.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="d-flex flex-column card rounded-lg">
+    <nuxt-link tag="div" :to="`/products/${item.id}`">
+    <v-card>
+        <img :src="item.image" class="img" />
+        <div class="foot">
+        <span class="title">{{item.name}}</span>
+        <p class="price">{{item.price}}$</p>
+        </div>
+    </v-card>
+    </nuxt-link>
+</div>
+</template>
+
+<script>
+export default {
+    props: {
+        item: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .card {
+        width: 300px;
+        display: block;
+        margin: 10px 0px;
+        /* border: 1px solid grey; */
+        cursor: pointer;
+    }
+    .img {
+        width: 100%;
+        height: 200px;
+        object-fit: cover;
+    }
+    .foot{
+        padding: 5px 10px;
+    }
+    .title{
+        font-size: 20px;
+    }
+    .price{
+        text-align: right;
+        color: #2E86AB;
+    }
+</style>
