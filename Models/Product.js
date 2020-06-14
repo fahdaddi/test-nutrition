1ab66770f9f30637317b55f14e648fefe200ca59
Fahd <fahd.addi@gmail.com>
2020-06-14 16:31:00 +0100

    product model

---
+++ Models/Product.js
@@ -0,0 +1,14 @@
+class Product {
+    constructor(id, name, price, gramesPerDoze, image, description, NutritionalDetails) {
+        this.id = id;
+        this.name = name
+        this.image = image;
+        this.description = description;
+        this.price = price;
+        this.gramesPerDoze = gramesPerDoze;
+        this.NutritionalDetails = NutritionalDetails;
+    }
+}
+
+
+export default Product;
